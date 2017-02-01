a600940495acc87c977fb2a16fd3f8bdc6637e57
SalvatoreMaraniello <salvatore.maraniello10@imperial.ac.uk>
2017-02-01 16:42:19 +0000

    Installation:
    Provided that Anaconda is installed on the system, the installation should now be almost automatic  on Ubuntu OS.
    
    The required C++ libraries Eigen and Boost can now be downloaded during the installation.

--- README.md
+++ README.md
@@ -6,71 +6,55 @@ Simulation of High-Aspect-Ratio Planes in Python (SHARPy).
 Nonlinear and consitently linearized models of very flexible aircraft dynamics
 for design, analysis, and control law synthesis.
 
-Install
--------
 
-1. Clone this git repository onto your local machine at location
-[SharPyProjectDir].
-2. Edit the file [SharPyProjectDir]/src/Main/SharPySettings.py so that
 
-	SharPyProjectDir = [SharPyProjectDir]
+### Install ###
 
-Note: The BeamLib shared library, BeamLib.so, is compiled automatically by
-unit tests in the PyBeam package.
-However, the UVLMLib.so library must be compiled manually at present;
-to do this import the UVLMLib project in eclipse (with the C/C++ SDK installed)
-by navigating to 'File->Import->General->Existing Projects into Workspace',
-and set [SharPyProjectDir]/UVLMLib as the root
-directory.
-Then build a Debug configuration of the project.
+1. Make sure Anaconda is installed (see below)
+2. From terminal, run 'python install.py'. See comments in install.py for more
+details and installation options.
+
+
 
 ### Dependencies ###
 
-* Python 3.2
-* numpy (1.7.x+)
-* scipy
-* h5py
-* slycot (routines for preditive controller design)
-* muAO-MPC (MPC controller design and C-code generation)
-* Eigen (C++ libraries)
-* Boost (C++ libraries)
-* LAPACK (Fortran libraries)
-* BLAS (Fortran libraries)
+- Anaconda3-2.3.0-Linux-x86-64 or higher: this contains all the python packages
+required to run SHARPy (see detailed list below)
+- gfortran compiler
+- C++ libraries Eigen and Boost. If not availale, these can be automatically 
+downloaded using install.py
 
-The fortran and C/C++ projects must be reconfigured to reflect the locations
-of these libraries on your system.
+- Details:
+    * Python 3.2
+    * numpy (1.7.x+)
+    * scipy
+    * h5py
+    * slycot (routines for preditive controller design)
+    * muAO-MPC (MPC controller design and C-code generation)
+    * Eigen (C++ libraries)
+    * Boost (C++ libraries)
+    * LAPACK (Fortran libraries)
+    * BLAS (Fortran libraries)
 
-Run
----
+During the installation, the fortran and C/C++ projects Makefiles are 
+reconfigured to reflect the locations of these libraries on your system.
+
+
+
+### Run ###
 
 The first thing you should do is run some python unittests
 which can be found in the test/ directory of PyAero, PyBeam and PyCoupled
 solver scripts.
 
 
+
 ### Installation Troubleshot ###
 
-- Anaconda3-2.3.0-Linux-x86-64 (https://repo.continuum.io/archive/) contains all the required python packages
+- see comments in install.py
 
-- ensure paths to all python files are set-up correctly. This is achieved adding this path:
-    ./SHARPy/src/Main/
-  to sys.path list.
 
-- The BeamLib.so can be compiled manually:
-    1. go to:
-        /home/sm6110/git/SHARPy/BeamLib/src/wrapper
-    2. type:
-        make all
-  
-- Run the shell script:
-    /home/sm6110/git/SHARPy/BeamLib/src/wrapper/f2py/runf2py.sh
-  The script relies on the F2PY, which is installed with numpy. If multiple python installations are icluded, ensure 
-  that the correct script is called by modifying the call to 'f2py3.2' to 'f2py3' or the full path of the script.
 
-- Eigen and boost installation:
-    1. You can try to download from: 
-        http://eigen.tuxfamily.org/index.php?title=Main_Page#Download 
-        http://sourceforge.net/projects/boost/?source=typ_redirect
-    and follow the instructions. Usually no compilation is required.
-    2. In the eclipse project/makefile include the path to X, where X is the folder containing Eigen/Dense. If no 
-    compilation was required, these are the folders you extracted
+### Uninstall ###
+- Remove the SHARPy directory from your system. 
+
--- install.py
+++ install.py
@@ -1,53 +1,219 @@
 
 '''
-SHARPy installation:
+SHARPy installation file
 
-prerequisites: 
-- Anaconda3-2.3.0-Linux-x86-64 or higher
+@author: Salvatore Maraniello
+@contact: salvatore.maraniello10@imperial.ac.uk
+@date: 1 Feb 2017
+@brief: SHARPy installation file for Ubuntu. Read instructions below.
+
+
+Prerequisites: 
+--------------
+- Anaconda3-2.3.0-Linux-x86-64 or higher (see below)
 - gfortran compiler
-- modify UVLMLib/MakeFile
-    EIGENDIR=PATHTO/eigen-eigen-3.2.5
-    BOOSTDIR=PATHTO/boost_1_58_0 
+- C++ libraries Eigen and Boost. If not availale, these can be downloaded using
+this script (see Instruction before).
 
 
+Instructions:
+-------------
+- Make sure you have installed Anaconda and check the name of the f2py 
+executable shipped with it is 'f2py3'. If not, modify the 'f2py_executable'
+variable accordingly. See also the troubleshooting section below.
+- In the default setting, this script will allow you to install the latest
+versions of the C++ libraries Eigen and Boost, required to compile the UVLM
+solver. If you wish to use your own versions of these libraries, or prefer to
+install them manually, set to False the 'DownloadEigen' and 'DownloadBoost' 
+variables below and specify their location through the EIGENDIR and BOOSTDIR
+variables.
+- If you also want to download SHARPy, set CloneRepo=True
+- The installer will try to modify the bash file ~/.bash_aliases. If this is
+not your bash file, or you prefer to modify this manually, set ModifyBash=False
+and follow the instructions below.
+- Once you are happy with this setting, navigate into the SHARPy folder ( or 
+to the path where you want to clone  the SHARPy folder) and run:
+	python install.py
+- The installer will try to add an environmental variable including the path to
+SHARPY into your bash file ~/.bash_aliases'. If an error occurs at this state,
+just add the following line 
+    export SHARPYDIR="your/path/to/SHARPy"
+to your bash file.
+
+
+Uninstall:
+----------
+- Remove the SHARPy directory from your system. 
+
+
+Installation Troubleshot: 
+------------
+- Finding the f2py executable name:
+To determine the executable name, you can simply type 'f2py' in terminal, press
+Tab and see at the suggested option. If required, use the 'whereis' command to 
+find the location of the executable (on Unix systems, these are typically placed 
+in /usr/bin or /usr/local/bin).
+- Add path to SHARPy in bash file:
+In Ubuntu systems you may want to add this to ~/.bash_aliases. To check that
+the setting is correct, once you have modified your bash file, open a new 
+terminal and type:
+    echo $SHARPYDIR	
+
+
+Installing Anaconda:
+--------------------
+Ref. https://docs.continuum.io/anaconda
+From command line:
+- Navigate to folder where you want to install Anaconda
+- wget https://repo.continuum.io/archive/Anaconda3-2.3.0-Linux-x86_64.sh
+- chmod u+x Anaconda3-2.3.0-Linux-x86_64.sh
+- ./Anaconda3-2.3.0-Linux-x86_64.sh
+- Follow installer instructions
+--------------------------------------------------------------------------------
 '''
 
 import os
 
-CloneRepo = False
 
-# clone repository
-if CloneRepo: os.system('git clone https://github.com/SalvatoreMaraniello/SHARPy')
-SHARPYDIR = os.getcwd() + '/SHARPy'
+# Input (optional):
+# -----------------
+# Include here the paths to the C++ libraries Eigen and Boost. If these are not 
+# installed on your system, the latest versions will be downloaded and installed
+# under the SHARPy directory.
+DownloadEigen=True
+DownloadBoost=True
+EIGENDIR=None
+BOOSTDIR=None
 
-# add to bash fine
-# export SHARPYDIR="/home/sm6110/git/SHARPy"
+# If you wish to clone SHARPy, set this to True 
+CloneRepo = True
+
+# Set the name of the f2py executable installed with Anaconda. 
+f2py_executable='f2py3'
+
+# Modify bash file
+ModifyBash=True
 
 
-# compile Fortran libraries
-os.chdir(SHARPYDIR+'/BeamLib/src/wrapper')
-os.system('mkdir -p %s' %(SHARPYDIR+'/bin'))
-os.system('make all')
+
+# Installer: do not modify!
+# ------------------------------------------------------------------------------
 
 
-# f2py run
-os.chdir(SHARPYDIR+'/BeamLib/src/wrapper/f2py')
-os.system('chmod u+x ./runf2py3x.sh')
-os.system('./runf2py3x.sh')
+# Clone repository
+# ----------------
+if CloneRepo: 
+    os.system('git clone https://github.com/SalvatoreMaraniello/SHARPy')
+    SHARPYDIR = os.getcwd() + '/SHARPy'
+else:
+    SHARPYDIR = os.getcwd()
+os.chdir(SHARPYDIR)
 
 
-# compile UVLM libraries
+# Install C++ libraries
+# ---------------------
+# Determine paths
+if EIGENDIR==None or BOOSTDIR==None: os.system('mkdir -p %s/ExtLib' %SHARPYDIR)
+if EIGENDIR==None: EIGENDIR= SHARPYDIR + '/ExtLib/eigen'
+if BOOSTDIR==None: BOOSTDIR= SHARPYDIR + '/ExtLib/boost_1_61_0'
+# Eigen
+if DownloadEigen:
+    os.chdir(SHARPYDIR + '/ExtLib')
+    os.system('hg clone https://bitbucket.org/eigen/eigen/')
+    print('Eigen library successfully downloaded into %s' %EIGENDIR)
+# Boost
+if DownloadBoost:
+    os.chdir(SHARPYDIR + '/ExtLib')
+    os.system('wget -O boost_1_61_0.tar.gz '
+              'http://sourceforge.net/projects/boost/files/boost/1.61.0/'
+              'boost_1_61_0.tar.gz/download')
+    os.system('tar zxvf boost_1_61_0.tar.gz')
+    os.system('rm boost_1_61_0.tar.gz')
+    print('Eigen library successfully downloaded into %s' %BOOSTDIR)	
+
+
+# Compile UVLM solver
+# -------------------
 os.chdir(SHARPYDIR+'/UVLMLib')
+# Read UVLM libraries Makefile
+MakeFileName='./Makefile'
+with open(MakeFileName, 'r') as file:
+    Lines = file.readlines()
+# Update paths to EIGEN/BOOST libraries
+for ii in range(len(Lines)):
+    if Lines[ii][:9]=="EIGENDIR=": 
+    	print(Lines[ii])
+    	Lines[ii]='EIGENDIR='+EIGENDIR+'\n'
+    	print(Lines[ii])
+    if Lines[ii][:9]=="BOOSTDIR=": 
+    	Lines[ii]='BOOSTDIR='+BOOSTDIR+'\n'
+# Overwrite Makefile
+with open(MakeFileName, 'w') as file:
+	for  line in Lines: file.write(line)
+# compile UVLM libraries
 os.system('make all')
 
+
+# Compile Fortran libraries
+# -------------------------
+os.chdir(SHARPYDIR+'/BeamLib/src/wrapper')
+os.system('mkdir -p %s' %(SHARPYDIR+'/BeamLib/bin'))
+os.system('make all')
+
+
+# Run f2py
+# --------
+os.chdir(SHARPYDIR+'/BeamLib/src/wrapper/f2py')
+#os.system('chmod u+x ./runf2py3.sh')
+#os.system('./runf2py3.sh')
+os.system('%s -h lib_fem_f2py.pyf -m lib_fem lib_fem_f2py.f90'
+	                                   ' --overwrite-signature'%f2py_executable)
+os.system('%s -c -m lib_fem lib_fem_f2py.pyf lib_fem_f2py.f90' %f2py_executable)
+os.system('%s -h lib_cbeam3_f2py.pyf -m lib_cbeam3 lib_cbeam3_f2py.f90'
+	                                   ' --overwrite-signature'%f2py_executable)
+os.system('%s -c -m lib_cbeam3 -I../ -L../../lib/src/ lib_cbeam3_f2py.pyf '
+	      'lib_cbeam3_f2py.f90 ../../lib/src/lib_fem.o ../../lib/src/lib_rot.o'
+	                             ' ../../lib/src/lib_rotvect.o'%f2py_executable)
+os.system('%s -h lib_rotvect_f2py.pyf -m lib_rotvect lib_rotvect_f2py.f90 '
+	                                    '--overwrite-signature'%f2py_executable)
+os.system('%s -c -m lib_rotvect -I../ -L../../lib/src/ lib_rotvect_f2py.pyf'
+	            ' lib_rotvect_f2py.f90 ../../lib/src/lib_rot.o'%f2py_executable)
+
+
+# Export SHARPYDIR
+# ----------------
+# This step may require sudo permissions
 
 # return to original directory
 os.chdir(SHARPYDIR)
 
-print('Installation almost completd...')
-print('1. Add export SHARPYDIR="......./SHARPy" to your bash file')
+if ModifyBash:
+    # Back up copy of bash file
+    homedir = os.path.expanduser('~')
+    try:
+        os.system('cp %s/.bash_aliases %s/.bash_aliases-sharpy.backup'
+                                                                   %(2*(homedir,)) )
+        with open('%s/.bash_aliases' %homedir, 'a') as file:
+    	    file.write('# SHARPY path:\n'
+    			       'export SHARPYDIR="%s"' %SHARPYDIR)
+    except:
+        print('Warning: I could not export SHARPy path to your bash file !!!')
+        print('Add the following line\n'
+    		  'export SHARPYDIR="%s"\n'
+    		  'to your bash file' %SHARPYDIR)
+
+print('Installation completed!')
 
 
+'''
+Manually install packages
+sudo apt-get install gfortran
+sudo apt-get install python3-f2py
+sudo apt-get install python3-dev
 
+sudo apt install mercurial
 
-
+sudo apt-get install python3-numpy
+sudo apt-get install python3-scipy
+sudo apt-get install python3-matplotlib
+'''
